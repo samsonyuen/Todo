5bb4f796270315e528bded28fa753e85f4cf90b5
Samson Yuen <samson.yuen@outlook.com>
2018-04-13 16:32:57 -0400

    Added code to delete tasks

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,12 @@
 $("li").on("click", function () {
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete To-Do
+$("span").on("click", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation(); // Stop bubbling up to the li. (Only want to fire on the span (span is part of li, but we don't want to fire on li)
+
+});
