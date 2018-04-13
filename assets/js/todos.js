5f7052477c7d06b1a13074fc86b52d604e3441c3
Samson Yuen <samson.yuen@outlook.com>
2018-04-13 16:55:32 -0400

    Added code to add new tasks (by pressing enter after entering task in text box). Also included some modifications to previous code for the click listeners to work for newly added elements.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,34 @@
 // Check off specific Todos By Clicking
-
+/*
 $("li").on("click", function () {
+    $(this).toggleClass("completed");
+});
+*/
+// The above code won't work for new li elements added.
+// The modification below is required because it is not
+//    allowed to add listeners to elements not yet on the page.
+// This fixed the problem because the listener is added to the ul
+// The "li" inside the on() indicates this fires when an li inside
+//    ul is clicked.
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
 // Click on X to delete To-Do
-$("span").on("click", function (event) {
-    $(this).parent().fadeOut(500, function () {
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(200, function () {
         $(this).remove();
     });
     event.stopPropagation(); // Stop bubbling up to the li. (Only want to fire on the span (span is part of li, but we don't want to fire on li)
 
+});
+
+$("input[type='text']").on("keypress", function (event) {
+    if (event.which === 13) {  // 13 corresponds to the enter key
+        // Extract text
+        var todoText = $(this).val();
+        // Make new li and add to ul
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        $(this).val("");
+    }
 });
